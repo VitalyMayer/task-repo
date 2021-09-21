8a4acb515cc279383ebe5a468d0bf367fc160a95
VitalyMayer <vitalymayer34@gmail.com>
2021-09-21 20:50:00 +0300

    Structure site

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My site</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <header></header>
+        
+        <nav><div>Меню</div>
+            <ul>
+                <li><a href="#">Lesson 1</a></li>
+                <li><a href="#">Lesson 2</a></li>
+            </ul>
+        </nav>
+        
+        <main></main>
+
+        <footer></footer>
+    </body>
+</html>
